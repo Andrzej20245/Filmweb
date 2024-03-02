81825c0cef8cf3afb6cb7772480abcc3e3312fe0
Andrzej20245 <andrzej20123454@gmail.com>
2024-03-02 15:54:50 +0100

    Dodanie aplikacji typu Filmweb w Django

---
+++ filmy/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Film(models.Model):
+    tytul = models.CharField(max_length=64, blank=False, unique=True)
+    rok = models.PositiveSmallIntegerField(blank=False)
+    opis = models.TextField(default="")
+    premiera = models.DateField(null=True, blank=True)
+    imdb_pkts = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.tytul, str(self.rok))
+
+class Aktorzy(models.Model):
+    nazwa = models.CharField(max_length=64, blank=False, unique=True)
+    wiek = models.PositiveSmallIntegerField(blank=False)
+    opis = models.TextField(default="")
+    urodzenie = models.DateField(null=True, blank=True)
+    imdb_pkts = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.nazwa, str(self.wiek))
